68c53c428ffc86d13801291fc9b5e4dc85941980
sh <1169283943@qq.com>
2022-05-15 09:52:59 +0800

    新加了add命令的三个作用

--- index.js
+++ index.js
@@ -5,3 +5,17 @@
 
 //查看状态精简命令 git status -s  /  git status --short
 // ?? 表示当前文件未被追踪的意思
+// A 表示当前文件已处于追踪文件
+
+// M 表示当前文件已经修改了
+
+//跟踪新文件 git add index.js   (tab键补全路径)
+// ↑ ↓ 箭头返回代码
+
+//git commit -m '描述信息'
+// -m 表示 message 的意思  命令后面的描述信息一定要写
+
+//已修改文件 modified 红色M
+
+//暂存已修改的文件
+// git add 命令
